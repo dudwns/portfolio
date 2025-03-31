91d7b485d3b2a7e4c5185de4a636eabd34a762e3
Yeongjun Kim <kyjun519@gmail.com>
2025-03-31 15:05:11 +0900

    refactor: 라우트 경로 변경

--- src/app/(route)/layout.tsx
+++ src/app/(route)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/common/Header/Header";
-import { ScrollProvider } from "./context/ScrollContext";
+import { ScrollProvider } from "../context/ScrollContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,12 +1,15 @@
 import About from "@/components/page/About";
 import Skills from "@/components/page/Skills";
 import Profile from "@/components/page/Profile";
+import Project from "@/components/page/Project";
+
 function Home() {
   return (
     <div className="max-w-400 mx-auto">
       <About />
       <Profile />
       <Skills />
+      <Project />
     </div>
   );
 }
