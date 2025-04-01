aff8cada809b1517cf121ac5070744426f6cb10d
Yeongjun Kim <kyjun519@gmail.com>
2025-04-01 21:06:36 +0900

    feat: 오버레이를 클릭했을 때 모달이 닫히도록 구현

--- src/components/modal/ModalOverlay.tsx
+++ src/components/modal/ModalOverlay.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function ModalOverlay({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
@@ -10,8 +12,14 @@ export default function ModalOverlay({ children }: { children: React.ReactNode }
     };
   }, []);
 
+  const handleOverlayClick = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      router.back();
+    }
+  };
+
   return (
-    <div className="fixed inset-0 bg-black/50 z-30">
+    <div className="fixed inset-0 bg-black/50 z-30" onClick={handleOverlayClick}>
       <div>{children}</div>
     </div>
   );
