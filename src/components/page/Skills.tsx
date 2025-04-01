24aafbbd889343d66221884087db132308c6efc7
Yeongjun Kim <kyjun519@gmail.com>
2025-04-01 18:09:16 +0900

    chore: 데이터 파일 경로 변경

--- src/components/page/Skills.tsx
+++ src/components/page/Skills.tsx
@@ -4,7 +4,7 @@ import { useInView, motion } from "framer-motion";
 import Image from "next/image";
 import { useRef } from "react";
 import SectionTitle from "../common/SectionTitle";
-import { SKILLS } from "@/constants/skills";
+import { SKILLS } from "@/app/data/skills";
 
 export default function Skills() {
   const ref = useRef<HTMLDivElement | null>(null);
