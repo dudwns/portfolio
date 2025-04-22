b324c94ab1683f2f072a3c2cd1268dba198932ab
Yeongjun Kim <kyjun519@gmail.com>
2025-04-22 19:23:41 +0900

    feat: Link 컴포넌트 제거

--- src/components/layout/Header/HeaderList.tsx
+++ src/components/layout/Header/HeaderList.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { SECTION_LIST } from "@/constants/section";
-import Link from "next/link";
 import { motion } from "framer-motion";
 import { useScrollSection } from "@/hooks/useScrollSection";
 import { useEffect, useState } from "react";
@@ -17,10 +16,11 @@ export default function HeaderList() {
     return (
       <ul className="flex gap-4 relative">
         {SECTION_LIST.map((list) => (
-          <li key={list.id} className="relative text-sm">
-            <Link href={list.id} className={`hover:text-white cursor-pointer text-gray-400`}>
-              {list.name}
-            </Link>
+          <li
+            key={list.id}
+            className="relative text-sm `hover:text-white cursor-pointer text-gray-400"
+          >
+            {list.name}
           </li>
         ))}
       </ul>
@@ -29,16 +29,14 @@ export default function HeaderList() {
   return (
     <ul className="flex gap-4 relative">
       {SECTION_LIST.map((list) => (
-        <li key={list.id} className="relative text-sm">
-          <Link
-            href={list.id}
-            onClick={(e) => scrollToSection(e, list.id)}
-            className={`hover:text-white cursor-pointer ${
-              activeSection === list.id ? "text-white" : "text-gray-400"
-            }`}
-          >
-            {list.name}
-          </Link>
+        <li
+          key={list.id}
+          onClick={(e) => scrollToSection(e, list.id)}
+          className={`relative text-sm hover:text-white cursor-pointer ${
+            activeSection === list.id ? "text-white" : "text-gray-400"
+          }`}
+        >
+          {list.name}
           {activeSection === list.id && (
             <motion.div
               layoutId="underline"
