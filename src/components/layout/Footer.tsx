4405f3dbb9c625f8abd5169247366d632a32b269
Yeongjun Kim <kyjun519@gmail.com>
2025-04-27 22:19:00 +0900

    style: Footer 배경색을 불투명하게 변경

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center text-foreground/90 text-sm h-30 flex items-center justify-center bg-foreground/10">
+    <footer className="text-center text-foreground/90 text-sm h-30 flex items-center justify-center relative bg-[#303033]">
       © 2025. YeongJun Kim. All rights reserved.
     </footer>
   );
