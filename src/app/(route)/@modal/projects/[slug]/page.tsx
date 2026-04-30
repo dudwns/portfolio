e4e4a7074d2dc2365abdd0b74c4a2e9f54b23bfe
Yeongjun Kim <kyjun519@gmail.com>
2026-04-30 17:26:43 +0900

    feat: 프로젝트 상세 페이지 정적 렌더링 적용

--- src/app/(route)/@modal/projects/[slug]/page.tsx
+++ src/app/(route)/@modal/projects/[slug]/page.tsx
@@ -6,6 +6,18 @@ type Props = {
   }>;
 };
 
+export async function generateStaticParams() {
+  return [
+    { slug: "linkhub" },
+    { slug: "angola" },
+    { slug: "mealkitary" },
+    { slug: "notion" },
+    { slug: "portfolio" },
+  ];
+}
+
+export const dynamicParams = false;
+
 export default async function ProjectModal({ params }: Props) {
   const { slug } = await params;
 
