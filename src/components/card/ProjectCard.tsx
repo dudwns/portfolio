126d1cc98e1b5ee4d8b482516002f11df984ae07
Yeongjun Kim <kyjun519@gmail.com>
2026-04-30 16:11:37 +0900

    feat: 프로젝트 상세 모달 렌더링 시 로딩 스피너 적용

--- src/components/card/ProjectCard.tsx
+++ src/components/card/ProjectCard.tsx
@@ -3,7 +3,9 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { SKILL_INFO } from "@/constants/projects";
+import Spinner from "@/components/spinner/Spinner";
 
 type ProjectCardProps = {
   image: string;
@@ -23,56 +25,62 @@ export default function ProjectCard({
   skills,
 }: ProjectCardProps) {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   return (
-    <motion.div
-      key={title}
-      className="flex flex-col w-full h-90 bg-foreground/10 mx-auto rounded-md shadow-md overflow-hidden relative group"
-      whileHover={{ y: -10 }}
-      transition={{ duration: 0.2 }}
-    >
-      <div className="relative w-full h-40 min-h-40">
-        <Image src={image} alt="project" fill className="object-cover" />
-      </div>
-      <div className="p-4 flex flex-col justify-between h-full">
-        <div className="flex flex-col gap-2">
-          <div className="flex justify-between items-center">
-            <p className="text-xl">{title}</p>
-            <span className="text-foreground/90 text-sm">
-              {isCollaborative ? "Team Project" : "Sing Project"}
-            </span>
-          </div>
-          <p className="text-sm">{description}</p>
-        </div>
-        <div className="flex gap-2 flex-wrap ">
-          {skills.map((skill) => {
-            const skillInfo = SKILL_INFO[skill];
-            return (
-              <span
-                key={skill}
-                className={`flex items-center gap-2 text-xs px-2 py-1 rounded-md  ${skillInfo.bgColor} ${skillInfo.color}`}
-              >
-                <skillInfo.icon className="w-4 h-4" />
-                {skill}
-              </span>
-            );
-          })}
-        </div>
-      </div>
+    <>
       <motion.div
-        className="absolute inset-0 bg-black/30 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-0.2"
-        initial={{ opacity: 0 }}
-        whileHover={{ opacity: 1 }}
+        key={title}
+        className="flex flex-col w-full h-90 bg-foreground/10 mx-auto rounded-md shadow-md overflow-hidden relative group"
+        whileHover={{ y: -10 }}
+        transition={{ duration: 0.2 }}
       >
-        <span
-          className="text-foreground text-lg  border border-foreground rounded-md px-2 py-1 cursor-pointer"
-          onClick={() => {
-            router.push(`/projects/${route}`, { scroll: false });
-          }}
+        <div className="relative w-full h-40 min-h-40">
+          <Image src={image} alt="project" fill className="object-cover" />
+        </div>
+        <div className="p-4 flex flex-col justify-between h-full">
+          <div className="flex flex-col gap-2">
+            <div className="flex justify-between items-center">
+              <p className="text-xl">{title}</p>
+              <span className="text-foreground/90 text-sm">
+                {isCollaborative ? "Team Project" : "Sing Project"}
+              </span>
+            </div>
+            <p className="text-sm">{description}</p>
+          </div>
+          <div className="flex gap-2 flex-wrap ">
+            {skills.map((skill) => {
+              const skillInfo = SKILL_INFO[skill];
+              return (
+                <span
+                  key={skill}
+                  className={`flex items-center gap-2 text-xs px-2 py-1 rounded-md  ${skillInfo.bgColor} ${skillInfo.color}`}
+                >
+                  <skillInfo.icon className="w-4 h-4" />
+                  {skill}
+                </span>
+              );
+            })}
+          </div>
+        </div>
+        <motion.div
+          className="absolute inset-0 bg-black/30 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-0.2"
+          initial={{ opacity: 0 }}
+          whileHover={{ opacity: 1 }}
         >
-          자세히 보기
-        </span>
+          <span
+            className="text-foreground text-lg  border border-foreground rounded-md px-2 py-1 cursor-pointer"
+            onClick={() => {
+              startTransition(() => {
+                router.push(`/projects/${route}`, { scroll: false });
+              });
+            }}
+          >
+            자세히 보기
+          </span>
+        </motion.div>
       </motion.div>
-    </motion.div>
+      {isPending && <Spinner />}
+    </>
   );
 }
